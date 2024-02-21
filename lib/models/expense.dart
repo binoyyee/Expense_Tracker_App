ef1294ebf1e8047061447711f5f7d478b5cf7263
Binoy <binoyvk99@gmail.com>
2024-02-21 19:09:20 +0530

    initial commit

---
+++ lib/models/expense.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart'; //Using 3rd Party Package to generate Unique ID
+import 'package:intl/intl.dart'; //Using 3rd PP to format system date time to readable
+
+final uuid = Uuid(); //Declared outside class so it is globally accessible
+
+final formatter = DateFormat.yMd();
+
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
+class Expense {
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
+  //Using Initializer Lists to initiate id  which is not received as constructor function argument.
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum = sum + expense.amount;
+    }
+    return sum;
+  }
+}
